69d7778c907cc8d8463ed74e1d5208b8ef4ce506
Manon Le <babyvert13@hotmail.com>
2020-12-23 19:56:25 +0100

    try CORS fix

--- src/app.js
+++ src/app.js
@@ -34,7 +34,8 @@ function buildApp() {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000' );
+        // res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS, PUT');
         next();
